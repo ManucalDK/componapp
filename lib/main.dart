49831fc23e259f19b3e5e06c36c79ede1c2e6c87
Manuel Alejandro Calambás <macalambas@gmail.com>
2020-08-10 23:05:44 -0500

    my first listview

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Componentes App',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('ComponAPP'),
-          ),
-          body: HomePageTemp()),
-    );
+        title: 'Componentes App',
+        debugShowCheckedModeBanner: false,
+        home: HomePageTemp());
   }
 }
